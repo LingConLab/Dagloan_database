d265d22720e726279e9683fe65922a7af8793e2d
Ilia <ilyachechuro@gmail.com>
2019-04-01 22:58:02 +0300

    Ilia added standardized transcription

--- Convert/standardize_transcription.py
+++ Convert/standardize_transcription.py
@@ -1,4 +1,4 @@
-import sys, re
+import sys, re, operator
 table = []
 metatable = []
 metadata = {}
@@ -14,7 +14,7 @@ for line in metafile:
 	metatable.append(line.strip("\r\n")) 
 for line in metatable[1:]:
 	a = line.split("\t")
-	metadata[a[0]] = a[2]
+	metadata[a[0]] = a[1]
 		
 for line in sys.stdin.readlines():
 	table.append(line.strip("\r\n")) 
@@ -22,14 +22,31 @@ meaninglist = {}
 #speaker_list = table[0].split("\t")[1:]
 
 #For Speaker as a column:
+
+transcription_frequency = {}
 for line in table[1:]:
 	a = line.split("\t")
-	if a[0] in meaninglist and a[2] in meaninglist[a[0]]:
-		meaninglist[a[0]][a[2]].append(a[4])
-	elif a[0] in meaninglist and a[2] not in meaninglist[a[0]]:
-		meaninglist[a[0]][a[2]] = [a[4]]
+	if str(a[0])+" "+str(a[4]) in transcription_frequency:
+		if a[3] in transcription_frequency[str(a[0])+" "+str(a[4])]:
+			transcription_frequency[str(a[0])+" "+str(a[4])][a[3]] += 1
+		else:
+			transcription_frequency[str(a[0])+" "+str(a[4])][a[3]] = 1
 	else:
-		meaninglist[a[0]] = {a[2]:[a[4]]}
+		transcription_frequency[str(a[0])+" "+str(a[4])] = {}
+		transcription_frequency[str(a[0])+" "+str(a[4])][a[3]] = 1
+
+good_transcriptions = {}
+for line in table[1:]:
+	a = line.split("\t")
+	if a[2] != "NA" and metadata[a[2]] == "Dictionary":
+		good_transcriptions[str(a[0])+" "+str(a[4])] = a[3] + " (Dictionary); " + max(transcription_frequency[str(a[0])+" "+str(a[4])].items(), key=operator.itemgetter(1))[0] + " (Frequency)"
+	else:
+		good_transcriptions[str(a[0])+" "+str(a[4])] = max(transcription_frequency[str(a[0])+" "+str(a[4])].items(), key=operator.itemgetter(1))[0] + " (Frequency)"
+
+#assigning dict IDs
+#for line in table[1:]:
+#	a = line.split("\t")
+#	if str(a[0])+" "+str(a[4])
 
 #For Speaker as a row:
 #for line in table[1:]:
@@ -44,24 +61,18 @@ for line in table[1:]:
 #print(meaninglist)
 
 #For Speaker as a column:
-header = 'Lexeme\t'
-result = '\n'
-used_ids = set()
-for concept in meaninglist:
-	result += str(concept) + "\t"	
-	for speaker in metadata:			
-		#header += str(metadata[speaker]) + "\t"
-		if speaker not in used_ids:
-			header += str(metadata[speaker]) + "\t"
-			used_ids.add(speaker)
-		#result += str(meaninglist[speaker][concept]) + "\t"
-		if speaker in meaninglist[concept]:
-			for word in meaninglist[concept][speaker]:
-				result += str(word) + " "
-			result += "\t"
-		else:
-			result += "NA\t"
-	result += "\n"
+result = ""
+for line in table[1:]:
+	a = line.split("\t")
+	for element in a:
+		result += str(element)+"\t"
+	result = result + good_transcriptions[str(a[0])+" "+str(a[4])]+"\n"
+
+header = ""
+for element in table[0].split("\t"):
+	header += element + "\t"
+header += "Standardized_Transcription\n"
+result = header + result	
 
 #For Speaker as a row:
 #header = 'Speaker\t'
@@ -81,7 +92,6 @@ for concept in meaninglist:
 #print(result)
 
 			
-result = header + result
 
 
 result = subst("NA ", "", result)
